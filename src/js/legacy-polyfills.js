a88831bac88761c22ea02b81b12280907a34e20a
@johnegraham2 <johnegraham2@gmail.com>
2018-04-09 16:31:02 -0400

    updated promise includes

--- src/js/legacy-polyfills.js
+++ src/js/legacy-polyfills.js
@@ -1,7 +1,4 @@
 import 'custom-event-polyfill';
-//import Promise from 'es6-promise-polyfill';
-//import 'es6-shim';
+import 'es6-shim';
 import 'nodelist-foreach-polyfill';
 import 'whatwg-fetch';
-
-window.Promise = Promise;
