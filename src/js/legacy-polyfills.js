b508ed270bcfc74f744d84ded4fa688eb0dd527c
@johnegraham2 <johnegraham2@gmail.com>
2018-04-09 17:44:02 -0400

    added promise.finally

--- src/js/legacy-polyfills.js
+++ src/js/legacy-polyfills.js
@@ -1,4 +1,7 @@
 import 'custom-event-polyfill';
 import 'es6-shim';
 import 'nodelist-foreach-polyfill';
+import promiseFinally from 'promise.prototype.finally';
 import 'whatwg-fetch';
+
+promiseFinally.shim();
